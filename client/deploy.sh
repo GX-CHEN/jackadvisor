416581b1442097fc9f7673ab401b1ff3d295629b
Gongxia Chen <chengongxia1990@gmail.com>
2020-09-24 08:09:36 +0000

    fix deployment script

--- client/deploy.sh
+++ client/deploy.sh
@@ -2,8 +2,9 @@ cd '/root/jackadvisor' || exit
 git pull
 
 # deploy frontend portion
-cd '/root/jackadvisor/jackadvisor-web' || exit
+cd '/root/jackadvisor/client' || exit
 npm install
 npm run build || exit
-sudo cp -r build/ /var/www/html/
+sudo rm -rf /var/www/html/jackadvisor
+sudo cp -r build/ /var/www/html/jackadvisor
 sudo service nginx reload
